967e93a8144519135d18700694cbd6f012d00948
jihye0418 <lykim0418@naver.com>
2022-07-10 16:55:16 +0900

    change security passwordEncoder

--- demo-spring-security-form/src/main/java/me/whiteship/demospringsecurityform/account/Account.java
+++ demo-spring-security-form/src/main/java/me/whiteship/demospringsecurityform/account/Account.java
@@ -1,5 +1,7 @@
 package me.whiteship.demospringsecurityform.account;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -47,7 +49,7 @@ public class Account {
 
     private String role;
 
-    public void encodePassword(){
-        this.password = "{noop}" + this.password;
+    public void encodePassword(PasswordEncoder passwordEncoder){
+        this.password = passwordEncoder.encode(this.password);
     }
 }
