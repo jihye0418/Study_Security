3990c1530702bcb065733ac53949e91a05e6a610
jihye0418 <lykim0418@naver.com>
2022-07-10 14:07:22 +0900

    spring security by inmemory

--- demo-spring-security-form/src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
+++ demo-spring-security-form/src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package me.whiteship.demospringsecurityform.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,5 +18,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         http.formLogin();
         http.httpBasic();
+    }
+
+    // user 정보 임의로 여러 개 설정하기
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("spring").password("{noop}123").roles("USER").and()
+                .withUser("admin").password("{noop}!@#").roles("ADMIN");
     }
 }
